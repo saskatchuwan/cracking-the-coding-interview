dac3f19e9433e9781755b55805139c29ecf0b506
Kathryn <kathryn.chu10@gmail.com>
2019-04-02 21:30:24 -0700

    Attempt at 08_02

--- javascript/chapter08_recursion_and_dynamic_programming/08_02_robotInAGrid.js
+++ javascript/chapter08_recursion_and_dynamic_programming/08_02_robotInAGrid.js
@@ -1,11 +1,21 @@
-function findPath(grid) {
-  
-}
+// function findPath(grid) {
+//   let path = [];
+
+//   return _recurse(row, col, grid);
+// }
+
+// function _recurse(row, col, grid) {
+//   if (grid[row][col] !== 'x') {
+
+//   }
+// }
 
 
-/* TEST */
-var grid = [
-  ['0', '0', '0', '0'],
-  ['0', 'x', '0', 'x'],
-  ['x', '0', '0', '0'],
-];
+// /* TEST */
+// const grid = [
+//   ['0', '0', '0', '0'],
+//   ['0', 'x', '0', 'x'],
+//   ['x', '0', '0', '0'],
+// ];
+
+// findPath(grid);

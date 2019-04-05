0d2877da78146c63199cfded2c7c22650fd79ded
Kathryn <kathryn.chu10@gmail.com>
2019-04-05 13:14:22 -0700

    Complete non-optimized 08_02

--- javascript/chapter08_recursion_and_dynamic_programming/08_02_robotInAGrid.js
+++ javascript/chapter08_recursion_and_dynamic_programming/08_02_robotInAGrid.js
@@ -1,21 +1,44 @@
-// function findPath(grid) {
-//   let path = [];
+/*
+This solution currently will only return one possible path through.
+It also does not currently integrate DP (memoization).
+*/
+function findPath(grid) {
+  let allPaths = [];
+  const endRow = grid.length - 1;
+  const endCol = grid[0].length - 1;
+  
+  const _recurse = function(row, col) {
+    //base case for when we reach the end of the grid
+    if (row === endRow && col === endCol) return [[row, col]];
+    
+    //check next step in row
+    if (row < endRow && grid[row + 1][col] !== 'x') {
+      let res = _recurse(row + 1, col);
 
-//   return _recurse(row, col, grid);
-// }
+      if (res) {
+        return allPaths.concat([[row,col]].concat(res));
+      }
+    }
 
-// function _recurse(row, col, grid) {
-//   if (grid[row][col] !== 'x') {
+    //check next step in column
+    if (col < endCol && grid[row][col + 1] !== 'x') {
+      let res = _recurse(row, col + 1);
+      if (res) {
+        return allPaths.concat([[row, col]].concat(res));
+      }
+    }
+    //will only execute false if the path is invalid;
+    return false;
+  };
 
-//   }
-// }
+  return _recurse(0, 0);
+}
 
+/* TEST */
+const grid = [
+  ['0', '0', '0', '0'],
+  ['0', 'x', '0', 'x'],
+  ['x', '0', '0', '0'],
+];
 
-// /* TEST */
-// const grid = [
-//   ['0', '0', '0', '0'],
-//   ['0', 'x', '0', 'x'],
-//   ['x', '0', '0', '0'],
-// ];
-
-// findPath(grid);
+console.log(findPath(grid));
